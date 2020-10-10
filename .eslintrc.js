334ed91a432e3fb2f0b7716114c7f2b56bd9511d
Sebastian Vittersø <sebastian.vitters@gmail.com>
2020-10-10 11:14:39 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,23 @@ module.exports = {
     parser: 'babel-eslint'
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'vue/no-async-in-computed-properties': 'warn',
+    'vue/no-dupe-keys': 'warn',
+    'vue/no-duplicate-attributes': 'warn',
+    'vue/no-side-effects-in-computed-properties': 'warn',
+    'vue/no-template-key': 'warn',
+    'vue/no-textarea-mustache': 'warn',
+    'vue/no-unused-components': 'warn',
+    'vue/no-unused-vars': 'warn',
+    'vue/no-use-v-if-with-v-for': 'warn',
+    'vue/require-prop-type-constructor': 'warn',
+    'vue/require-v-for-key': 'warn',
+    'vue/require-valid-default-prop': 'warn',
+    'vue/use-v-on-exact': 'warn',
+    'vue/valid-v-else-if': 'warn',
+    'vue/valid-v-else': 'warn',
+    'vue/valid-v-for': 'warn'
   }
 }
